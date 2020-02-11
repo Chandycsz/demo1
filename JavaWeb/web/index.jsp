2a99ecae22e00ab5902e2c7f81aaa4d774e95edb
Chandycsz <13967724141@163.com>
2020-02-11 20:49:58 +0800

    Initial commit

---
+++ JavaWeb/web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: asus
+  Date: 2020/2/11
+  Time: 18:05
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>6666</title>
+  </head>
+  <body>
+   <h1 style="color: red">61661115</h1>
+  </body>
+</html>
---
+++ src/demo.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.Date;
+
+public class demo {
+    public static final int HIGH = 1;
+
+    public static void main(String[] args) {
+
+
+        System.out.println("6666");
+        System.out.println("HelloWorld!!!");
+
+        ArrayList a = new ArrayList();
+        new Date();
+        System.out.println("111");
+        String[] s = new String[]{"Tom","Jerry","Lilei"};
+        for (int i = 0; i < s.length; i++) {
+            System.out.println(s[i]);
+        }
+
+
+
+
+    }
+    public static void method(){
+
+    }
+}
+class lll{
+    int i;
+    int t;
+
+}
